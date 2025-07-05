b9b87c4913548c538e010d471c29b82ddb4d4053
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-05 21:59:33 +0700

    fixing deserialization on scan_toml.rs to dotfile on .config

--- src/config/scan_toml.rs
+++ src/config/scan_toml.rs
@@ -1,8 +1,18 @@
 use serde::Deserialize;
 use std::fs;
-use std::path::Path;
 use std::path::PathBuf;
-use toml::Table;
+//use toml::Table;
+
+#[derive(Deserialize, Debug)]
+struct TomlFile {
+    items: Items,
+}
+
+#[derive(Deserialize, Debug)]
+struct Items {
+    first: String,
+    second: String,
+}
 
 pub fn scan_toml(config_path: &PathBuf) {
     assert!(config_path.exists(), "example.toml does not exist");
@@ -14,4 +24,10 @@ pub fn scan_toml(config_path: &PathBuf) {
         .collect::<String>();
 
     println!("{file}");
+    
+    let file_str = std::fs::read_to_string(config_path).unwrap();
+    let tomlfile: TomlFile = toml::from_str(&file_str).unwrap();
+
+    println!("{}", tomlfile.items.first);
+    println!("{}", tomlfile.items.second);
 }
