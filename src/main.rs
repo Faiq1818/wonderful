5cfb301f308b026bce0ddbff09111624ebc31886
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-15 20:15:58 +0700

    ignore return from draw function in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,15 @@ mod config;
 mod draw;
 mod input;
 
-//use color_eyre::Result;
-
 fn main() {
-    //color_eyre::install()?;
     let mut terminal = ratatui::init();
-
     let config_path = config::check_config_path::get_dotconfig_path("wonderful", "wonderful.toml");
     config::check_config_path::ensure_config_exists(&config_path);
-
     let mut selected = 0;
-
     let items = config::scan_toml::get_items_name(&config_path);
 
     loop {
-        terminal.draw(|f| draw::draw::draw(f, selected, &items));
+        let _ = terminal.draw(|f| draw::draw::draw(f, selected, &items));
         
         if input::keyboard_push::keyboard_push(&mut selected, &items, &config_path).expect("REASON") {
             break;
