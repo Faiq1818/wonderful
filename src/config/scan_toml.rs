7a6a60853bb2dfbd1079ffe4ec95212a1657db29
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-16 01:02:13 +0700

    fixing the section value key

--- src/config/scan_toml.rs
+++ src/config/scan_toml.rs
@@ -1,8 +1,7 @@
 use std::process::Command;
 use toml;
 
-pub type TomlFile = std::collections::HashMap<String, std::collections::HashMap<String, String>>;
-pub type TomlFile2 = std::collections::BTreeMap<String, std::collections::BTreeMap<String, String>>;
+pub type TomlFile = std::collections::BTreeMap<String, std::collections::BTreeMap<String, String>>;
 
 pub fn scan_toml(config_path: &std::path::PathBuf, selected_item: &str) {
     assert!(config_path.exists(), "example.toml does not exist");
@@ -10,9 +9,11 @@ pub fn scan_toml(config_path: &std::path::PathBuf, selected_item: &str) {
     let file_str = std::fs::read_to_string(config_path).unwrap();
     let parsed: TomlFile = toml::from_str(&file_str).expect("invalid toml structure");
 
-    for kvs in parsed.values() {
-        for (key, val) in kvs {
-            if selected_item == key {
+    for (section, kvs) in &parsed {
+        println!("{}", section);
+        if selected_item == section {
+            for (key, val) in kvs {
+                println!("{}",key);
                 let args: Vec<&str> = val.split_whitespace().collect();
                 if !args.is_empty() {
                     let _ = Command::new(args[0]).args(&args[1..]).spawn();
@@ -24,16 +25,14 @@ pub fn scan_toml(config_path: &std::path::PathBuf, selected_item: &str) {
 
 pub fn get_items_name(config_path: &std::path::PathBuf) -> Vec<String> {
     let file_str = std::fs::read_to_string(config_path).unwrap();
-    let parsed: TomlFile2 = toml::from_str(&file_str).expect("invalid toml structure");
+    let parsed: TomlFile = toml::from_str(&file_str).expect("invalid toml structure");
 
-    let mut items = vec![];
+    //let mut items = vec![];
+    let items = parsed.keys().cloned().collect::<Vec<_>>();
     for kvs in parsed.values() {
-        for key in kvs.keys() {
-            let args: Vec<&str> = key.split_whitespace().collect();
-
-            for s in args {
-                items.push(s.to_string());
-            }
+        //items.push(kvs.to_string());
+        for _key in kvs.keys() {
+            //items.push(key.to_string());
         }
     }
     items
