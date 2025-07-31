6a1a2b0e402f430c547ffb0b426596a3517433f5
sinavarasina <varasinafarmadani@gmail.com>
2025-07-31 07:24:58 +0700

    add little comment for better documentation, lmfao

--- src/logic/search.rs
+++ src/logic/search.rs
@@ -1,3 +1,6 @@
+// NOTE: this is expected to be an non-CASE-SENSITIVE by iterating each string and turn every
+//       string to lower case, u can see at .filter bla bla bla *.to_lowercase(), lemme know if you want something diffrent
+//       (for example regex), lol.
 pub fn filter_items<'a>(all_items: &'a [String], find_string: &str) -> Vec<&'a String> {
     all_items
         .iter()
